5e6a9ce48bffc3b24ca560c40c472ec6e1e64b27
jvo <mrjhonyvidal@gmail.com>
2024-12-25 23:40:02 +0000

    Add new theme color #4ca154 Shacdn + assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
+import Image from "next/image";
+import Logo from "../assets/logo.svg";
+import LandingImg from "../assets/main.svg";
 import { Button } from "@/components/ui/button";
-import { Camera } from "lucide-react";
+import Link from "next/link";
 
-const HomePage = () => {
+export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Button>Default Button </Button>
-      <Button variant="outline" size="icon">
-        <Camera> </Camera>
-      </Button>
-    </div>
+    <main>
+      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
+        <Image src={Logo} alt="Logo" />
+      </header>
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+        <div>
+          <h1 className="capitalize text-4xl md:text-7xl font-bold">
+            job <span className="text-primary">tracking</span> app
+          </h1>
+          <p className="leading-loose max-w-md mt-4">
+            Your Job Hunt, Simplified. Track. Perfect. Succeed.
+          </p>
+          <Button asChild className="mt-4">
+            <Link href="/add-job">Get Started</Link>
+          </Button>
+        </div>
+        <Image
+          src={LandingImg}
+          alt="Landing Image"
+          className="hidden lg:block"
+        />
+      </section>
+    </main>
   );
-};
-export default HomePage;
+}
